a0e8875ac5b4dede16db5b200727cc7b03379936
Hamza Ali <hamzamalimaad@gmail.com>
2022-07-20 15:04:26 +0400

    update

--- src/main/java/com/hamzaali/springauthenticationserver/config/UserManagementConfig.java
+++ src/main/java/com/hamzaali/springauthenticationserver/config/UserManagementConfig.java
@@ -1,11 +1,14 @@
 package com.hamzaali.springauthenticationserver.config;
 
 
+import com.hamzaali.springauthenticationserver.repository.UserRepository;
 import com.hamzaali.springauthenticationserver.services.UsersDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,10 +19,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class UserManagementConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public UserDetailsService uds (){
+    @Autowired
+    UsersDetailsService usersDetailsService;
 
-        return new UsersDetailsService();
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usersDetailsService);
     }
 
     @Bean
--- src/main/java/com/hamzaali/springauthenticationserver/entities/User.java
+++ src/main/java/com/hamzaali/springauthenticationserver/entities/User.java
@@ -1,21 +1,24 @@
 package com.hamzaali.springauthenticationserver.entities;
 
-import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-
+    @Column
     private String username;
+    @Column
     public  String password;
 }
